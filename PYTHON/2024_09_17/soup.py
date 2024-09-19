2ee4654c1751f03f339ec7f92742680481bea712
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-09-20 01:40:51 +0200

    Create soup.py

---
+++ PYTHON/2024_09_17/soup.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+cnn_url = "https://www.cnn.com/"
+
+def scrape_with_beautiful_soup(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    #Extract and print headlines
+    headlines = soup.select('span')
+    for headline in headlines:
+        print(headline.text)
+
+#Scrape headlines using Beautiful Soup
+
+scrape_with_beautiful_soup(cnn_url)
