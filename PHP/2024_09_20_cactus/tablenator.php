03f8b112ff7ec1573eb1e2eaa2e7f4d493534b1f
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-09-25 22:38:51 +0200

    Update tablenator.php

--- PHP/2024_09_20_cactus/tablenator.php
+++ PHP/2024_09_20_cactus/tablenator.php
@@ -1,1 +1,28 @@
+<form method="post">
+  <label for="rows">Filas:</label><br>
+  <input type="text" id="rows" name="rows"><br>
+  <label for="cols">Columnas:</label><br>
+  <input type="text" id="cols" name="cols"><br>
+  <input type="submit" value="Enviar">
+</form>
 
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_POST["rows"]) && isset($_POST["cols"])) {
+                $rows = htmlspecialchars($_POST["rows"]);
+                $cols = htmlspecialchars($_POST["cols"]);
+
+
+                echo '<table border="1">';
+                for ($i = 0; $i < $rows; $i++) {
+                        echo "<tr>";
+                        for ($j = 0; $j < $cols; $j++) {
+                                echo "<td>DAW2</td>";
+                        }
+                        echo "</tr>";
+                }
+                echo "</table>";
+
+        }
+}
+?>
